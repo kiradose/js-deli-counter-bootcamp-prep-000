750056e6430eba3a2c8e1e970af284137a3f53fc
Clay G <gclaista@gmail.com>
2019-09-13 04:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ function currentLine(line){
     return "The line is currently empty.";
   }
   var lineNamesandNumbers = [];
-  
+
   for(var i=0; i<line.length; i++) {
     lineNamesandNumbers.push(i+1 + ". "+ line[i]);
   }
   console.log("The line is currently: " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
-
