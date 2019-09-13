cce66182e8d1d0a47b86ea915d6b2ecd6d1a9bec
Clay G <gclaista@gmail.com>
2019-09-13 04:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function takeANumber(katzDeli , name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+
+function nowServing(x) {
+  if (x.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = x[0];
+    x.splice(0, 1);
+    return "Currently serving " + name + ".";
+  }
+}
